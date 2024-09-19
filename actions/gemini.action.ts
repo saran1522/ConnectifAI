af68299becd157997067240a1be8131e14ff5f1f
saran sinha <saransinha1522@gmail.com>
2024-09-19 20:19:03 +0530

    minor change

--- actions/gemini.action.ts
+++ actions/gemini.action.ts
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 if (!apiKey) throw new Error("Gemini API key is not valid");
 const genAI = new GoogleGenerativeAI(apiKey);
-const conversationHistory = [];
+let conversationHistory: any[] = [];
 
 export async function runConversation(query: string) {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
