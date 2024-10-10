9463bb89e1db25500acbb5551415993e03daf223
saran sinha <saransinha1522@gmail.com>
2024-10-10 18:12:32 +0530

    added error handling in server action

--- actions/gemini.action.ts
+++ actions/gemini.action.ts
@@ -2,23 +2,26 @@
 
 import { Content, GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const apiKey = process.env.GEMINI_API_KEY;
 if (!apiKey) throw new Error("Gemini API key is not valid");
 const genAI = new GoogleGenerativeAI(apiKey);
 let conversationHistory: Content[] = [];
 
 export async function runConversation(query: string) {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  conversationHistory.push({ role: "user", parts: [{ text: query }] });
-  const result = await model.generateContent({
-    contents: conversationHistory,
-  });
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    conversationHistory.push({ role: "user", parts: [{ text: query }] });
+    const result = await model.generateContent({
+      contents: conversationHistory,
+    });
 
-  const response = result.response;
-  const text = response.text();
-  conversationHistory.push({
-    role: "model",
-    parts: [{ text: response.text() }],
-  });
-  return conversationHistory;
+    const response = result.response;
+    conversationHistory.push({
+      role: "model",
+      parts: [{ text: response.text() }],
+    });
+    return conversationHistory;
+  } catch (error) {
+    console.error(error);
+  }
 }
